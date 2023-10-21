6eaf92b1751de615db536b4fabfffb33483b6dce
gufranky <g2676536169@gmail.com>
2023-10-21 10:32:44 +0800

    first

---
+++ zhishu_for'/zhishu_for.cpp
@@ -0,0 +1,22 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	for (int i = 2; i <= 100; i++)
+	{
+		bool check=true;
+		for (int j = 2; j * j <= i; j++)
+		{
+			if (i % j == 0)
+			{
+				check=false;
+			}
+		}
+		if (check)
+		{
+			cout<<i<<endl;
+		}
+	}
+}
---
+++ zhishu_for/zhishu_while.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i = 2;
+    while (i <= 100)
+    {
+        int j = 2;
+        bool check=true;
+        while (j * j <= i)
+        {
+            if (i % j == 0)
+            {
+                check=false;
+            }
+            j++;
+        }
+        if (check)
+        {
+            cout << i << endl;
+        }
+		i++;
+    }
+}
+
